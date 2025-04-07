935be212276d9b582f4c126e9014d614448454b2
pc <pc@DESKTOP-H8KKV86>
2025-04-08 00:37:25 +0100

    Ajoutez des fichiers projet.

---
+++ générationdétiquettes/Models/BarcodeEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace générationdétiquettes.Models
+{
+    public class BarcodeEntity
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string Base64Image { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+
+}
